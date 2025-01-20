eab7f0391f2e12ff583b255d017c1805e6cf40e4
Emma-Asoliya <e.briggs@alustudent.com>
2025-01-20 22:04:36 +0200

    task 0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
+"""Minimum Operations"""
 
-"""
-Given a number n, write a method that calculates the fewest # of operations.
-
-Prototype: def minOperations(n)
-Returns an integer
-If n is impossible to achieve, return 0
-"""
 
 def minOperations(n):
+    """Return number of min. operations to
+    copy and paste a single character 'H' to give
+    the required n characters of 'H'.
+
+    arg: `n`
+    """
+
     if n <= 1:
         return 0
 
-    i = 2
-    while i * i <= n:
-        if n % i == 0:
-            return i + minOperations(n // i)
-        i += 1
+    text_file_chars = 'H'
 
-    return n
+    copied_text = text_file_chars
+    op_count = 1
+
+    # while length of text_file chars is less than n
+    while len(text_file_chars) < n:
+        if len(text_file_chars) != 1 and not n % len(text_file_chars):
+            # if n is perfectly divisible by the len of our text char
+            # copy text and append to text_file
+            copied_text = text_file_chars
+            op_count += 1
+
+        # paste
+        text_file_chars += copied_text
+        op_count += 1
+
+    return op_count
+
+
+if __name__ == '__main__':
+    res = minOperations(2147483640)
+    print('minimum operations for 2147483640 chars is:', res)
